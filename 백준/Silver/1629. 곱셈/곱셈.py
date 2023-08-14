081ab600e5c9d57147b89ba872d7987ae68e4000
DongPark987 <55174232+DongPark987@users.noreply.github.com>
2023-08-14 15:40:26 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -7,12 +7,17 @@ A, B, C = map(int, input().split())
 
 def rec(B):
     if B == 1:
+        # A^1 mod C
         return A % C
 
     k = rec(B // 2) % C
     if B % 2 == 0:
+        #k => A//2 mod C
+        #((A^B//2 mod C) * (A^B//2 mod C)) mod C
         return k * k % C
     else:
+        # k => A//2 mod C
+        # ((A^B//2 mod C) * (A^B//2 mod C) * (A^1 mod C)) mod C
         return k * k % C * A % C
 
 
