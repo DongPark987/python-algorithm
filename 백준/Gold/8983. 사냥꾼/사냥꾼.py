afaf54592d8bf8065891229cda94bdb417cb4cde
DongPark987 <55174232+DongPark987@users.noreply.github.com>
2023-08-14 21:37:25 +0900

    [Gold IV] Title: 사냥꾼, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -0,0 +1,39 @@
+import sys
+
+input = sys.stdin.readline
+global L
+M, N, L = map(int, input().split())
+
+global hunter, animal, ans
+hunter = []
+animal = []
+ans = 0
+
+hunter = list(map(int, input().split()))
+hunter.sort()
+for _ in range(N):
+    animal.append(list(map(int, input().split())))
+# print(animal)
+# print(hunter)
+
+def search(left, right, x, y):
+    if (left > right):
+        return
+    global ans,hunter, animal, L
+    mid = (left + right) // 2
+    distance = abs(hunter[mid] - x) + y
+    if (distance <= L):
+        ans += 1
+        return
+    else:
+        if x < hunter[mid]:
+            search(left, mid - 1, x, y)
+        elif x > hunter[mid]:
+            search(mid + 1, right, x, y)
+        else:
+            return
+
+
+for i in animal:
+    search(0, M, i[0], i[1])
+print(ans)
