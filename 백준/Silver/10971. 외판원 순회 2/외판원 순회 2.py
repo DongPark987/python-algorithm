f544fe10f9d3c7a66de9620c15ee58d2808f846c
DongPark987 <55174232+DongPark987@users.noreply.github.com>
2023-08-15 16:12:53 +0900

    [Silver II] Title: 외판원 순회 2, Time: 152 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,41 +1,33 @@
 from itertools import combinations, permutations
 import sys
+import math
+
 input = sys.stdin.readline
 print = sys.stdout.write
 
-global N
+global N, arr, visit, ans, start
 N = int(input().rstrip())
-global arr
-arr = []
-global visit
+arr = [list(map(int, input().rstrip().split())) for _ in range(N)]
 visit = [False for i in range(N)]
-global ans
-ans = 1000000000
-global Start
+ans = math.inf
 Start = 0
 
-for _ in range(N):
-    arr.append(list(map(int, input().rstrip().split())))
 
+def dfs(loc, sum, before):
+    global arr, visit, ans, Start
 
-def dfs(loc, sum, pre):
-    global arr
-    global visit
-    global ans
-    global Start
-
-    if (loc == N-1):
-        if arr[pre][Start] != 0:
-            sum += arr[pre][Start]
+    if (loc == N - 1):
+        if arr[before][Start] != 0:
+            sum += arr[before][Start]
             ans = min(ans, sum)
             return
         return
     for i in range(N):
-        if arr[pre][i] == 0 or sum + arr[pre][i] >= ans:
+        if arr[before][i] == 0 or sum + arr[before][i] >= ans:
             continue
-        if visit[i] == False:
+        if not visit[i]:
             visit[i] = True
-            dfs(loc+1, sum + arr[pre][i], i)
+            dfs(loc + 1, sum + arr[before][i], i)
             visit[i] = False
 
 
@@ -44,6 +36,5 @@ for i in range(N):
     visit[i] = True
     dfs(0, 0, i)
     visit[i] = False
-
 
 print(str(ans))
