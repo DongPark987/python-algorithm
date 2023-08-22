6811d5049f6023250be8b3bb7977429cdee8eb46
DongPark987 <55174232+DongPark987@users.noreply.github.com>
2023-08-22 21:57:58 +0900

    [Gold V] Title: 최소비용 구하기, Time: 264 ms, Memory: 47656 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,5 +1,6 @@
 from collections import deque
 import math
+import heapq
 import sys
 
 input = sys.stdin.readline
@@ -15,18 +16,39 @@ for _ in range(M):
     a, b, c = map(int, input().split())
     city[a].append(b)
     cost[a][b] = min(cost[a][b], c)
-# print(city)
+
 start, end = map(int, input().split())
 
-visit = [math.inf for _ in range(N + 1)]
-q = deque()
-q.append(start)
-visit[start] = 0
-while q:
-    cursor = q.popleft()
-    for Next in city[cursor]:
-        if visit[Next] > visit[cursor] + cost[cursor][Next]:
-            visit[Next] = visit[cursor] + cost[cursor][Next]
-            q.append(Next)
+
+hq = []
+ans = [math.inf for i in cost[start]]
+ans[start] = 0
+heapq.heappush(hq, (0, start))
+while hq:
+    cursor = heapq.heappop(hq)
+    if ans[cursor[1]] != cursor[0]: continue
+    for i in city[cursor[1]]:
+        if ans[i] > cost[cursor[1]][i] + cursor[0]:
+            ans[i] = cost[cursor[1]][i] + cursor[0]
+            heapq.heappush(hq, (ans[i], i))
+
+# print(ans)
+print(ans[end])
+
+
+
+
+# #===========================
+# visit = [math.inf for _ in range(N + 1)]
+# q = deque()
+# q.append(start)
+# visit[start] = 0
+# while q:
+#     cursor = q.popleft()
+#     for Next in city[cursor]:
+#         if visit[Next] > visit[cursor] + cost[cursor][Next]:
+#             visit[Next] = visit[cursor] + cost[cursor][Next]
+#             q.append(Next)
+# # print(visit)
 # print(visit)
-print(visit[end])
+# print(visit[end])
