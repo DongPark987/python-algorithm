e145f1298256d0b86556ed5695a1a824879f32eb
DongPark987 <55174232+DongPark987@users.noreply.github.com>
2023-08-23 16:51:51 +0900

    [Gold V] Title: 동전 2, Time: 488 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -7,15 +7,15 @@ coin = []
 for _ in range(n):
     a = int(input())
     coin.append(a)
-ans = [math.inf] * 10001
+ans = [999999] * 10001
 ans[0] = 0
 
-for i in range(1, k + 1):
+for won in range(1, k + 1):
     for j in range(n):
-        if i - coin[j] >= 0:
-            ans[i] = min(ans[i], ans[i - coin[j]] + 1)
+        if won - coin[j] >= 0:
+            ans[won] = min(ans[won], ans[won - coin[j]] + 1)
 
-if ans[k] == math.inf:
+if ans[k] == 999999:
     print(-1)
 else:
     print(ans[k])
