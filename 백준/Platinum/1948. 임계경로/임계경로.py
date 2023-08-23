63a92ae8a5624c2dcf71b535a71a4742932f2b9b
DongPark987 <55174232+DongPark987@users.noreply.github.com>
2023-08-23 21:50:38 +0900

    [Platinum V] Title: 임계경로, Time: 5016 ms, Memory: 56320 KB -BaekjoonHub

---
+++ 백준/Platinum/1948. 임계경로/임계경로.py
@@ -0,0 +1,58 @@
+from collections import deque
+import sys
+import copy
+
+n = int(input())
+m = int(input())
+
+Link = [[] for _ in range(n + 1)]
+RLink = [[] for _ in range(n + 1)]
+parentCnt = [0 for _ in range(n + 1)]
+
+for i in range(m):
+    a, b, c = map(int, input().split())
+    Link[a].append([b, c])
+    RLink[b].append([a, c])
+    parentCnt[a] += 1
+
+# print(RLink)
+
+start, end = map(int, input().split())
+
+q = deque()
+q.append(end)
+MaxLength = [0 for _ in range(n + 1)]
+MaxLength[end] = 0
+subPCnt = copy.deepcopy(parentCnt)
+MaxPerson = []
+while q:
+    cur = q.popleft()
+    for Next, Cost in RLink[cur]:
+        MaxLength[Next] = max(MaxLength[Next], MaxLength[cur] + Cost)
+        subPCnt[Next] -= 1
+        if Next == start:
+            MaxPerson.append([Next, cur, MaxLength[cur] + Cost])
+        if subPCnt[Next] == 0:
+            q.append(Next)
+
+# print(MaxLength)
+# print(MaxPerson)
+
+# for cur, Next, cost in MaxPerson:
+#     if cost == MaxLength[start]:
+#         mySet.add((cur, Next))
+#         q.append(Next)
+ans = 0
+q.append(start)
+visit = [False] * (n + 1)
+while q:
+    cur = q.popleft()
+    for Next, Cost in Link[cur]:
+        if MaxLength[cur] - Cost == MaxLength[Next]:
+            ans += 1
+            if not visit[Next]:
+                q.append(Next)
+                visit[Next] = True
+
+print(MaxLength[start])
+print(ans)
