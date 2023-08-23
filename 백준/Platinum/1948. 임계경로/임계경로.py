28634d1edd4947ce1f880cf5d87162bc6d2ebae5
DongPark987 <55174232+DongPark987@users.noreply.github.com>
2023-08-23 22:36:59 +0900

    [Platinum V] Title: 임계경로, Time: 312 ms, Memory: 56252 KB -BaekjoonHub

--- 백준/Platinum/1948. 임계경로/임계경로.py
+++ 백준/Platinum/1948. 임계경로/임계경로.py
@@ -1,6 +1,7 @@
 from collections import deque
 import sys
-import copy
+
+input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
@@ -23,25 +24,17 @@ q = deque()
 q.append(end)
 MaxLength = [0 for _ in range(n + 1)]
 MaxLength[end] = 0
-subPCnt = copy.deepcopy(parentCnt)
 MaxPerson = []
 while q:
     cur = q.popleft()
     for Next, Cost in RLink[cur]:
         MaxLength[Next] = max(MaxLength[Next], MaxLength[cur] + Cost)
-        subPCnt[Next] -= 1
+        parentCnt[Next] -= 1
         if Next == start:
             MaxPerson.append([Next, cur, MaxLength[cur] + Cost])
-        if subPCnt[Next] == 0:
+        if parentCnt[Next] == 0:
             q.append(Next)
 
-# print(MaxLength)
-# print(MaxPerson)
-
-# for cur, Next, cost in MaxPerson:
-#     if cost == MaxLength[start]:
-#         mySet.add((cur, Next))
-#         q.append(Next)
 ans = 0
 q.append(start)
 visit = [False] * (n + 1)
