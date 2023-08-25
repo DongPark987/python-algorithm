8ac26285341f66cdfdfa8f47d90b51be3b259ba5
DongPark987 <55174232+DongPark987@users.noreply.github.com>
2023-08-25 22:05:16 +0900

    [Gold V] Title: 동전 2, Time: 432 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -3,18 +3,19 @@ import math
 
 n, k = map(int, input().split())
 
-coin = []
+coin = set()
 for _ in range(n):
     a = int(input())
-    coin.append(a)
+    coin.add(a)
 ans = [999999] * 10001
 ans[0] = 0
 
 for won in range(1, k + 1):
-    for j in range(n):
-        if won - coin[j] >= 0:
-            ans[won] = min(ans[won], ans[won - coin[j]] + 1)
+    for c in coin:
+        if won - c >= 0:
+            ans[won] = min(ans[won], ans[won - c] + 1)
 
+# print(ans)
 if ans[k] == 999999:
     print(-1)
 else:
