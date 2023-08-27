9b0cb43b38fb8bf6aa5ad37a96104fd9760f0d94
DongPark987 <55174232+DongPark987@users.noreply.github.com>
2023-08-27 19:38:38 +0900

    [Silver I] Title: 회의실 배정, Time: 5008 ms, Memory: 58420 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,22 @@
+N = int(input())
+
+arr = []
+for _ in range(N):
+    arr.append(list(map(int, input().split())))
+
+arr.sort(key=lambda x: (x[0], x[1]))
+
+# for i in range(N):
+#     print(arr[i])
+
+cur = arr[0]
+ans = 1
+for i in range(1, len(arr)):
+    if cur[1] <= arr[i][0]:
+        ans += 1
+        cur = arr[i]
+    else:
+        if cur[1] > arr[i][1]:
+            cur = arr[i]
+print(ans)
+
