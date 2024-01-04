b44ecec4fbd78abd02a7cccb6dfe81af0b806a82
DongPark987 <55174232+DongPark987@users.noreply.github.com>
2024-01-04 22:52:50 +0900

    [Bronze I] Title: 소인수분해, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N = 0;
+	cin >> N;
+
+	for (int i = 2; i <= N; i++) {
+		while (N % i == 0) {
+			cout << i << '\n';
+			N /= i;
+		}
+	}
+}
